53f03153d104e74145f1e944089cd08f9892221a
Akshay Kadam <akshaykadam0904@gmail.com>
2023-11-16 20:47:02 +0530

    update code to add aws package necessary for r2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
+const webpack = require("webpack")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/google',
-        destination: 'https://google.com/',
-        permanent: true,
-      },
-    ]
-  },
+	webpack: (config, { isServer, nextRuntime }) => {
+		// Avoid AWS SDK Node.js require issue
+		if (isServer && nextRuntime === "nodejs")
+			config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ }))
+		return config
+	},
+	redirects: async () => {
+		const redirects = [
+			{
+				source: "/google",
+				destination:
+					"https://google.com/",
+				permanent: true,
+			},
+		]
+		return redirects
+	},
 }
 
 module.exports = nextConfig
